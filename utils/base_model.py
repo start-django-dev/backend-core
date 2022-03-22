db951ac1b13d7e9bed9e5b6410fc2b7cc89048fd
cafitac <cafitac99@gmail.com>
2022-03-23 03:33:26 +0900

    feat: AbstractDateTime 모델 클래스를 생성합니다

---
+++ utils/base_model.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class AbstractDateTime(models.Model):
+    """
+    생성 시간, 수정 시간에 대해 저장하는 추상 모델
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
