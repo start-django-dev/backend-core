b09d643e19a496858dfe067978b89252d76c9cff
cafitac <cafitac99@gmail.com>
2022-03-23 04:01:09 +0900

    test: Provision 에 대한 테스트 코드를 추가합니다

--- provision/tests/test_create_project_api.py
+++ provision/tests/test_create_project_api.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from provision.models.provision import Provision
+
 
 class CreateProjectAPITest(TestCase):
     """
@@ -11,14 +13,20 @@ class CreateProjectAPITest(TestCase):
         payload = {
             "python_version": "3.8",
             "django_version": "3.2.12",
-            "project_name": "",
+            "project_name": "django_project",
         }
 
         # when
         res = self.client.post(
-            "/api/generate-project/", data=payload, content_type="application/json"
+            path="/api/provision/",
+            data=payload,
+            content_type="application/json",
         )
 
         # then
         self.assertEqual(res.status_code, 201)
         # self.assertIsInstance(res.content)
+        provision_obj: Provision = Provision.objects.get()
+        self.assertEqual(provision_obj.python_version, payload["python_version"])
+        self.assertEqual(provision_obj.django_version, payload["django_version"])
+        self.assertEqual(provision_obj.project_name, payload["project_name"])
