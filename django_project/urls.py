b1fd1d8b3f1452acec90a1363441885dcbf72250
cafitac <cafitac99@gmail.com>
2022-03-23 03:59:36 +0900

    fix: Provision 앱에 대한 라우팅 주소 에러를 수정합니다

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/api/provision/", include("provision.urls")),
+    path("api/provision/", include("provision.urls")),
 ]
