d159b2bc3165be61835bccfccfc475f550241a02
cafitac <cafitac99@gmail.com>
2022-03-23 03:40:59 +0900

    feat: ProvisionAPIView 에 대한 라우팅을 진행합니다

--- provision/urls.py
+++ provision/urls.py
@@ -1,3 +1,14 @@
-# from django.urls import path
+from django.urls import path
 
-urlpatterns = []
+from provision.api.provision import ProvisionAPIView
+
+urlpatterns = [
+    path(
+        "",
+        ProvisionAPIView.as_view(
+            {
+                "post": "create",
+            }
+        ),
+    ),
+]
